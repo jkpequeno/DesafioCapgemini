e0b5cd50a9ac24de9dfadb61dc64435ed7b6c1e9
jkpequeno <85908076+jkpequeno@users.noreply.github.com>
2022-03-08 05:52:29 -0300

    Update DesafioUm.java

--- Desafio Capgemini/src/desafio/DesafioUm.java
+++ Desafio Capgemini/src/desafio/DesafioUm.java
@@ -2,14 +2,14 @@ package desafio;
 
 import java.util.Arrays;
 
-public class Um {
+public class DesafioUm {
 
 	public static void main(String[] args) {
 		int[] arr = { 9, 2, 1, 4, 6, 8, 5, 7, 3};
 		int index = arr.length/2;
 		Arrays.sort(arr);
 		
-		System.out.println("A mediana para o array em memória, com número ímpar de elementos é "+ arr[index]);
+		System.out.println("A mediana para o array em memÃ³ria, com nÃºmero Ã­mpar de elementos Ã© "+ arr[index]);
 
 	}
 }
